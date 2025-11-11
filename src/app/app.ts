aee9d4f247fff3f0a2ef3a4bf11ed9bb869bac18
MedinaDz <I.indetectableAI@gmail.com>
2025-11-11 01:21:38 -0500

    Configuración de HTPP y base de la interfaz del usuario recontra actualizado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('HotelFrontend');
+  protected readonly title = signal('🏨 Hotel Cuenca');
 }
